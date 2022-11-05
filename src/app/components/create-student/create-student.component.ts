45eb22376670fe7ecbabae298170f32f390038be
Praveen-kumar8096 <praveenpalepu8096@gmail.com>
2022-11-05 17:13:37 +0530

    form implemented with personal information

--- src/app/components/create-student/create-student.component.ts
+++ src/app/components/create-student/create-student.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormControlName, FormGroup, FormGroupName } from '@angular/forms';
 
 @Component({
   selector: 'app-create-student',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-student.component.css']
 })
 export class CreateStudentComponent implements OnInit {
+  public studentForm:FormGroup=new FormGroup(
+  {
+    name:new FormControl(),
+    gender:new FormControl(),
+    mobile:new FormControl(),
+    email:new FormControl(),
+    batch:new FormControl(),
+    address: new FormGroup(
+      {
+        city:new FormControl(),
+        state: new FormControl(),
+        pin:new FormControl()
+      }
+    )
 
+  },
+
+  )
   constructor() { }
 
   ngOnInit(): void {
   }
+  register(){[
+    console.log(this.studentForm)
+  ]}
 
 }
